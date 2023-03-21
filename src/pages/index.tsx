3aa31cc281a4465ca6abe28d48a980f167bb4bd1
vanor-cardozo <vanor.fc@gmail.com>
2023-03-21 11:01:29 -0300

    feat: Estrutura Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,29 @@
-import { styled } from "../styles"
-
-const Button = styled('button', {
-  backgroundColor: "$rocketseat",
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import tshirt1 from '../assets/tshirts/1.png'
+import tshirt2 from '../assets/tshirts/2.png'
+import tshirt3 from '../assets/tshirts/3.png'
+import tshirt4 from '../assets/tshirts/4.png'
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt="camiseta com estampa de um foguete decolando"/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={tshirt2} width={520} height={480} alt="camiseta com estampa de um foguete decolando"/>
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
