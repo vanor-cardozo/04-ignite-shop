f45d54ff5cae95c679a1723f6382b2df852bd5f5
vanor-cardozo <vanor.fc@gmail.com>
2023-03-21 11:17:55 -0300

    feat: Implementa o carrossel para os produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Image from "next/image";
+
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from "../styles/pages/home";
+
 import tshirt1 from '../assets/tshirts/1.png'
 import tshirt2 from '../assets/tshirts/2.png'
 import tshirt3 from '../assets/tshirts/3.png'
 import tshirt4 from '../assets/tshirts/4.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={tshirt1} width={520} height={480} alt="camiseta com estampa de um foguete decolando"/>
 
         <footer>
@@ -16,8 +29,26 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={tshirt2} width={520} height={480} alt="camiseta com estampa de um foguete decolando"/>
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="camiseta com estampa de um foguete decolando"/>
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt4} width={520} height={480} alt="camiseta com estampa de um foguete decolando"/>
 
         <footer>
           <strong>Camiseta Y</strong>
