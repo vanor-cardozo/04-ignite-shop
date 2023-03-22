6a1cac571691a028fdd81242235d4b4f33aac9c3
vanor-cardozo <vanor.fc@gmail.com>
2023-03-22 10:27:37 -0300

    feat: Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 
 export default function Product() {
     const {query} = useRouter()
     return(
-        <h1>{JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum repellendus architecto, quae excepturi perspiciatis fugiat a blanditiis consequatur quam expedita velit assumenda sint quos modi quia dolorem, repudiandae voluptas sit?</p>
+
+                <button>
+                    comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
