8f3c5f95623ebe70343da78aa39dad025e9bb31a
vanor-cardozo <vanor.fc@gmail.com>
2023-03-22 14:41:43 -0300

    feat: Estrutura página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
     return(
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Compra Efetuada!</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Uhuull <strong>Vanor Cardozo</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+            </p>
+
+            <Link href={'/'}>
+                voltar ao Catalogo
+            </Link>
+        </SuccessContainer>
     )
 }
