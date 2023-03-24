c47eab2ff055e0485aade2bf28748a286356c699
vanor-cardozo <vanor.fc@gmail.com>
2023-03-24 11:28:27 -0300

    feat: Configuração de estilização para os elementos

--- src/components/CustomerCart.tsx
+++ src/components/CustomerCart.tsx
@@ -1,10 +1,11 @@
 import { HandbagSimple } from 'phosphor-react'
 import * as Dialog from '@radix-ui/react-dialog'
-import { CartClose, CartContent, CartFooter, CartItemContainer, CartItemText } from '../styles/components/CustomerCart'
+import { CartClose, CartContainer, CartContent, CartFooter, CartItemContainer, CartItemText } from '../styles/components/CustomerCart'
 import { X } from 'phosphor-react'
 import Image from 'next/image'
 
 export function CustomerCart() {
+    const repeat = [0,1,2,3,4,5,6,7,8,9]
     return(
         <Dialog.Root>
             <Dialog.Trigger asChild>
@@ -21,27 +22,31 @@ export function CustomerCart() {
                         <X size={24} weight="bold" />
                     </CartClose>
                     <h2>Carrinho de compras</h2>
-                    <CartItemContainer>
-                        <Image
-                            src={""}
-                            alt=""
-                            width={102}
-                            height={94}
-                        />
-                        <CartItemText>
-                        <p>Camiseta Beyond the Limits</p>
-                        <strong>R$ 79,90</strong>
-                        <button>Remover</button>
-                        </CartItemText>
-                    </CartItemContainer>
+                    <CartContainer>
+                        {repeat.map((item)=> (
+                            <CartItemContainer>
+                            <Image
+                                src={""}
+                                alt=""
+                                width={102}
+                                height={94}
+                            />
+                            <CartItemText>
+                            <p>Camiseta Beyond the Limits</p>
+                            <strong>R$ 79,90</strong>
+                            <button>Remover</button>
+                            </CartItemText>
+                        </CartItemContainer>
+                        ))}
+                    </CartContainer>
                     <CartFooter>
                         <span>
                             <p>Quantidade</p>
                             <p>3 itens</p>
                         </span>
                         <span>
-                            <p>Valor total</p>
-                            <p>3 itens</p>
+                            <strong>Valor total</strong>
+                            <strong>R$ 199,90</strong>
                         </span>
                         <button>
                             Finalizar compra
--- src/styles/components/CustomerCart.ts
+++ src/styles/components/CustomerCart.ts
@@ -17,18 +17,35 @@ export const CartClose = styled(Dialog.Close, {
     position: 'fixed',
     top: 20,
     right: 20,
+    transition: '0.3s',
+    cursor: 'pointer',
 
     '&:hover': {
         color: '$green300'
     }
 })
 
+export const CartContainer = styled('div',{
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'space-between',
+    overflowY: 'scroll',
+    marginTop: '2rem',
+
+    '&::-webkit-scrollbar': {
+        display: 'none',
+    }
+    
+})
+
 export const CartItemContainer = styled('div', {
     display: 'flex',
     gap: '1rem',
-    marginTop: '2rem',
+    marginBottom: '2rem',
 
     img: {
+        borderRadius: 8,
         objectFit: 'cover',
         background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
     }
@@ -67,13 +84,46 @@ export const CartItemText = styled('div', {
 export const CartFooter = styled('div', {
     display: 'flex',
     flexDirection: 'column',
-    alignContent: 'flex-end',
-    background: '$gray500',
-    marginTop: '400px',
-
+    
     span: {
         display: 'flex',
         alignItems: 'baseline',
         justifyContent: 'space-between',
+
+        '&:first-child': {
+            marginTop: '1rem',
+
+            'p + p': {
+                fontSize: '1.125rem',
+                color: '$gray300'
+            }
+        },
+
+        strong: {
+            fontSize: '1.125rem',
+            color:'$gray100',
+            marginBottom: '1rem',
+
+            '&:last-child': {
+                fontSize: '1.5rem',
+            }
+        }
+    },
+
+    button: {
+        border: 0,
+        borderRadius: 8,
+        paddingBottom: '1rem',
+        paddingTop: '1rem',
+        background: '$green500',
+        fontSize: '1.125rem',
+        fontWeight: 'bold',
+        color: '$white',
+        cursor: 'pointer',
+        transition: '0.3s',
+
+        '&:hover': {
+            background: '$green300',
+        }
     }
 })
